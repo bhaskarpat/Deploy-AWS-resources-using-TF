03be81b834bea3863d9ea5d6ee0deffb92150c47
bhaskarpat <107925858+bhaskarpat@users.noreply.github.com>
2022-09-15 22:25:46 -0700

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet1_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "Web Subnet 1"
 }
@@ -13,7 +13,7 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet2_cidr}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "Web Subnet 2"
 }
@@ -23,7 +23,7 @@ resource "aws_subnet" "application-subnet-1" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet3_cidr}"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "Application Subnet 1"
 }
@@ -34,7 +34,7 @@ resource "aws_subnet" "application-subnet-2" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet4_cidr}"
   map_public_ip_on_launch = false
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "Application Subnet 2"
 }
@@ -43,7 +43,7 @@ resource "aws_subnet" "application-subnet-2" {
 resource "aws_subnet" "database-subnet-1" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet5_cidr}"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "Database Subnet 1"
 }
@@ -52,7 +52,7 @@ resource "aws_subnet" "database-subnet-1" {
 resource "aws_subnet" "database-subnet-2" {
   vpc_id = "${aws_vpc.demovpc.id}"
   cidr_block = "${var.subnet6_cidr}"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "Database Subnet 1"
 }
