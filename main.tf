afcc7e93216897ddd663e492d0810f57543fd378
bhaskarpat <107925858+bhaskarpat@users.noreply.github.com>
2022-09-15 22:21:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
-  access_key = "give your key"
-  secret_key = "give your account key"
+  region = "us-east-1"
+  access_key = "AKIAXNUHIET7D6W2LP6N"
+  secret_key = "szbEZQCSqVrrVTE/PAsl2CBHK/MR5/Hseyp3DRHr"
 }
